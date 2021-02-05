97582d238e172e42c64ac06648736e956c36c4b6
Akamya <75986500+Akamya@users.noreply.github.com>
2021-02-05 09:15:11 +0100

    Add files via upload

---
+++ Activity1.cs
@@ -0,0 +1,36 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App_Xamarin_Intent.Resources.layout
+{
+    [Activity(Label = "Activity1")]
+    public class Activity1 : Activity
+    {
+        Button BTN;
+        TextView TXT;
+        int Num;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "layout1" layout resource
+            SetContentView(Resource.Layout.layout1);
+            // Create your application here
+
+            TXT = FindViewById<TextView>(Resource.Id.txt1);
+            BTN = FindViewById<Button>(Resource.Id.btn1);
+            BTN.Click += delegate
+            {
+                TXT.Text = "Click : " + Num;
+                Num++;
+            };
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using App_Xamarin_Intent.Resources.layout;
+
+namespace App_Xamarin_Intent
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        Button BTN; 
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            BTN = FindViewById<Button>(Resource.Id.BtnChange);
+            BTN.Click += delegate
+            {
+                var intent = new Intent(this, typeof(Activity1));
+                StartActivity(intent);
+            };
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
